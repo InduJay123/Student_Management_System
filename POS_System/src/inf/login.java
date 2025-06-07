455c5c46e369b6958ddbd18c393f5f725806ba1d
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2025-06-07 15:07:11 +0530

    Update login.java

--- POS_System/src/inf/login.java
+++ POS_System/src/inf/login.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package inf;
 
-
-
-/**
- *
- * @author AWISHKA
- */
 public class login extends javax.swing.JFrame {
 
     /**
